729b9d159b289293e7a1e8385e788c3a6c6841ac
JazminneC <jazminne2127@gmail.com>
2025-12-04 11:34:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 console.log("Inizio esecuzione");
-function elabora() : {
-  let numero = document.getElementById("inNumber").value;
-  alert(numero);
+let = new Date();
+console.log(d);
+document.getElementById("saluto").innerHTML = d.getDay();
+
+function elabora() {
+    let numero = document.getElementById("inNumber").value;
+    alert(numero);
+
+    if (numero => 0 && numero <= 255) {
+        document.getElementById("outText").innerHTML = "Hai inserito il numero" + numero;
+    } else {
+        document.getElementById("error").innerHTML = "Il numero non è corretto";
+    }
+}
+
+function reset() {
+    document.getElementById("inNumber").value = "";
 }
